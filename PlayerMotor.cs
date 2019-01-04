b98e06a5c6e6216836a7164404a3b787b0cbcbc3
Ralph Duran <37376585+bstante@users.noreply.github.com>
2019-01-05 07:30:34 +0800

    Add files via upload

---
+++ PlayerMotor.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMotor : MonoBehaviour {
+
+    private CharacterController controller;
+    private Vector3 moveVector;
+
+    private float speed = 5.0f;
+    private float gravity = 12.0f;
+    private float verticalVelocity = 0.0f;
+    private float animationDuration = 2.0f;
+    void Start ()
+    {
+        controller = GetComponent<CharacterController>();
+	}
+
+
+    void Update()
+    {
+        if (Time.time < animationDuration)
+        {
+            controller.Move(Vector3.forward * speed * Time.deltaTime);
+            return;
+        }
+
+            moveVector = Vector3.zero;
+            if (controller.isGrounded)
+            {
+                verticalVelocity = -0.5f;
+            }
+            else
+            {
+                verticalVelocity -= gravity * Time.deltaTime;
+            }
+
+            moveVector.x = Input.GetAxisRaw("Horizontal") * speed;
+            moveVector.y = verticalVelocity;
+            moveVector.z = speed;
+
+            controller.Move(moveVector * Time.deltaTime);
+        }
+}
+
